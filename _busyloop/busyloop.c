b5a229a44c6c0480a3d59069ac717ad14e3a99ab
Oleg Levin <oleg.levin@yotadevices.com>
2017-05-17 12:11:39 +0300

    Busyloop for hw virtualization test

--- _busyloop/busyloop.c
+++ _busyloop/busyloop.c
@@ -1,22 +1,40 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+
+#include <sys/time.h>
 
 volatile int gIter = 0;
-
-int iteration() { return gIter++; }
 
 static inline void run_cpuid(int code, uint32_t* a, uint32_t* d) {
     asm volatile ( "cpuid" : "=a"(*a), "=d"(*d) : "0"(code) : "ebx", "ecx" );
 }
 
-int main() {
+int iteration() { 
 	uint32_t a = 0, d = 0;
-	run_cpuid(228, &a, &d);
-	printf("0x%x 0x%x\n", a, d);
+	run_cpuid(0xdeadbeef, &a, &d);
+	// printf("0x%x 0x%x\n", a, d);
+	return gIter++; 
+}
 
-	/* while (1) {
-		iteration();
-	} */
+int main(int argc, char** argv) {
+	uint32_t end;
+	struct timeval tv1, tv2;
+
+    if (argc == 2) {
+        end = strtol(argv[1], NULL, 10);
+        printf("%d\n", end);
+    } else {
+        end = 100;
+    }
+
+	gettimeofday(&tv1, NULL);
+	while (iteration() < end);
+	gettimeofday(&tv2, NULL);
+
+	printf ("Total time = %f seconds\n",
+         (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
+         (double) (tv2.tv_sec - tv1.tv_sec));
 
 	return 0;
 }
